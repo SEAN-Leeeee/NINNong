f711272b8f841d5e6b43a18c95e3d4814ad8ad50
sean <siyeon0805@kakao.com>
2025-01-21 23:09:36 +1300

    Initial project setup

---
+++ src/main/java/com/NINNong/nonggu/NongguApplication.java
@@ -0,0 +1,13 @@
+package com.NINNong.nonggu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NongguApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NongguApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/NINNong/nonggu/NongguApplicationTests.java
@@ -0,0 +1,13 @@
+package com.NINNong.nonggu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NongguApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
