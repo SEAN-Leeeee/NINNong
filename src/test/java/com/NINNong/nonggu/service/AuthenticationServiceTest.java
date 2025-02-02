e879babfb5ba5a0152a8afc43bc38d17f562b9da
sean <siyeon0805@kakao.com>
2025-02-02 21:11:15 +1300

    테스트 코드 수정

--- src/test/java/com/NINNong/nonggu/service/AuthenticationServiceTest.java
+++ src/test/java/com/NINNong/nonggu/service/AuthenticationServiceTest.java
@@ -25,54 +25,78 @@ class AuthenticationServiceTest {
     @Autowired
     private MemberRepository memberRepository;
 
+    private String email = "siyeon@naver.com";
+    private String name = "siyeon";
+    private String rawPassword = "1234";
+    private MemberEntity savedMember;
+
     @BeforeEach
-    public void beforeEach() {
-        System.out.println("test start!");
-    }
-
-    @AfterEach
-    public void afterEach() {
-        System.out.println("test end!");
-    }
-
-    @Test
-    @DisplayName("Sign up Succeeds when all fields are filled")
-    public void ifAllFieldsAreFilledThenSignUpShouldSucceed() {
-        //given
-        String email = "siyeon@naver.com";
-        String name = "siyeon";
-        String rawPassword = "1234";
-
+    public void setUp() {
         String encodedPassword = passwordEncoder.encode(rawPassword);
 
         Member member = new Member(email, name, encodedPassword);
         MemberEntity memberEntity = MemberEntity.fromDomain(member);
-
-        //When
-        MemberEntity savedMemberEntity = memberRepository.save(memberEntity);
-
-        //then
-        assertNotEquals(rawPassword, savedMemberEntity.getPassword(), "Password should be encrypted");
-
-        assertNotNull(savedMemberEntity.getId(), "MemberEntity Id Should not be null after saving");
-        assertEquals(email, savedMemberEntity.getEmail(), "email should match");
-        assertEquals(name, savedMemberEntity.getName(), "name should match");
-
-        MemberEntity foundMemberEntity = memberRepository.findById(savedMemberEntity.getId())
-                .orElseThrow(() -> new IllegalArgumentException("Member not found in repository"));
-
-        assertEquals(savedMemberEntity.getEmail(), foundMemberEntity.getEmail(), "Stored email should match");
-        assertEquals(savedMemberEntity.getName(), foundMemberEntity.getName(), "Stored name should match");
-
-      /*  PasswordEncoder encoder = new PasswordEncoder();
-
-        //when
-        String encodedPassword = encoder.encode(rawPassword);
-        Member member = new Member(email, name, encodedPassword);
-        MemberEntity memberEntity = memberEntity.fromDomain(member);
-
-        memberRepository.save(memberEntity);*/
-        //then
-        // 뭘 검증할 수 있을까??
+        savedMember = memberRepository.save(memberEntity);
     }
+
+    @AfterEach
+    public void tearDown() {
+        memberRepository.deleteAll();
+    }
+
+    private MemberEntity getMemberEntity(String email) {
+       return memberRepository.findByEmail(email)
+                .orElseThrow(() -> new IllegalStateException("Member not found"));
+    }
+
+    @Test
+    @DisplayName("Password should be encrypted")
+    public void passwordShouldBeEncrypted() {
+        // when
+        MemberEntity member = getMemberEntity(email);
+        //then
+        assertNotEquals(rawPassword, member.getPassword());
+        assertTrue(passwordEncoder.matches(rawPassword, member.getPassword()));
+
+    }
+
+    @Test
+    @DisplayName("MemberEntity Id Should not be null after saving")
+    public void savedMemberShouldNotBeNull() {
+
+        //then
+        assertNotNull(savedMember);
+    }
+
+    @Test
+    @DisplayName("email should match")
+    public void emailShouldMatch() {
+
+        //then
+        assertEquals(email, getMemberEntity(email).getEmail());
+    }
+
+    @Test
+    @DisplayName("name should match")
+    public void nameShouldMatch() {
+        //then
+        assertEquals(name,  getMemberEntity(email).getName());
+    }
+
+    @Test
+    @DisplayName("Stored email should match")
+    public void storedEmailShouldMatch() {
+        //then
+        assertEquals(savedMember.getEmail(), getMemberEntity(email).getEmail());
+    }
+
+    @Test
+    @DisplayName("Stored name should match")
+    public void storedNameShouldMatch() {
+
+        //then
+        assertEquals(savedMember.getName(), getMemberEntity(email).getName());
+    }
+
+
 }
